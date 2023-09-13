e7e4ae97cb8d881d00ece36ec2169606e9a8629b
Shion Uemura <130330406+Shion0927@users.noreply.github.com>
2023-09-13 12:20:36 +0900

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_sample',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
+    }
+}
